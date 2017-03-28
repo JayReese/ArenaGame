11a51151154a819c4a24be4604652ccc9b275d5d
Jay Reese <reese.j.jordon@gmail.com>
2017-03-28 00:22:41 -0500

    Did some things for the UI - changed it to reflect the stats you have.

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using System;
@@ -8,15 +9,31 @@ delegate void DisplayDamage(List<int> damagesDealt);
 public class UIManager : MonoBehaviour
 {
     [SerializeField] UIDisplay InterfaceDisplay;
-
+    [SerializeField] Image StatDisplayRenderer;
+    [SerializeField] Text StatDisplayText;
+    [SerializeField] Dictionary<string, Color> StatDisplayColorMeanings;
+    [SerializeField]
+    sbyte test;
 
 
     // Use this for initialization
     void Start()
     {
+        test = 4;
         InterfaceDisplay = GameObject.FindGameObjectWithTag("UI").GetComponent<UIDisplay>();
+        StatDisplayRenderer = GameObject.FindGameObjectWithTag("UI.StatDisplayBoard").GetComponent<Image>();
+        StatDisplayText = GameObject.FindGameObjectWithTag("UI.StatNumber").GetComponent<Text>();
+
+        PreallocateDisplayColors();
+
+        Debug.Log(StatDisplayColorMeanings["Neutral"]);
 
         Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        CorrectDisplayStat();
     }
 
     public void GiveDamageReport(RaycastHit hit, int damageDealt)
@@ -27,5 +44,39 @@ public class UIManager : MonoBehaviour
     void CreateCrosshair()
     {
         
+    }
+
+    // Testing method.
+    void CorrectDisplayStat()
+    {
+        StatDisplayText.text = Mathf.Abs(test).ToString();
+
+        switch(test <= 6 && test >= -6)
+        {
+            case true:
+                if (test > 0 || test < 0)
+                {
+                    if (test > 0) StatDisplayRenderer.color = StatDisplayColorMeanings["Positive"];
+                    else if (test < 0) StatDisplayRenderer.color = StatDisplayColorMeanings["Negative"];
+                }
+                else
+                    StatDisplayRenderer.color = StatDisplayColorMeanings["Neutral"];
+                break;
+            default:
+                test = 0;
+                break;
+        }
+        
+    }
+
+    void PreallocateDisplayColors()
+    {
+        StatDisplayColorMeanings = new Dictionary<string, Color>();
+
+        Vector3[] c = new Vector3[] { new Vector3(0.1294f, 0.3019f, 0.6901f), new Vector3(0, 0.6980f, 0), new Vector3(0.6901f, 0, 0) };
+        string[] s = new string[] { "Neutral", "Positive", "Negative" };
+
+        for (byte i = 0; i < c.Length; i++)
+            StatDisplayColorMeanings.Add(s[i], new Color(c[i].x, c[i].y, c[i].z));
     }
 }
